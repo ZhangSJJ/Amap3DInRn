a62e13f3df212e2a522ff10ddadea6591fbd94a0
zhangsjj <2012zhangsj@sina.cn>
2017-06-03 18:40:58 +0800

    update userinfo api finish

--- server/ApiServer/apiServer.js
+++ server/ApiServer/apiServer.js
@@ -2,7 +2,9 @@
  * Created by sjzhang on 2017/4/8.
  */
 var express = require('express');
+var bodyParser = require('body-parser');
 var apiServer = express();
+apiServer.use(bodyParser.urlencoded({extended: false}))//extended为false表示使用querystring来解析数据，这是URL-encoded解析器
 ///=======路由信息 （接口地址）开始 存放在./routes目录下===========//
 var users = require('./routes/User/users'); //用户接口
 apiServer.use('/users', users);//在app中注册users接口
--- server/ApiServer/routes/User/users.js
+++ server/ApiServer/routes/User/users.js
@@ -3,13 +3,13 @@
  */
 var URL = require('url');
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
-var TextData = require("../../TextData")
-router.get('/getUserInfo', function (req, res, next) {
+var UserInfoData = require("../data/UserInfoData.json");
+router.get('/getUserInfo', function (req, res) {
 	var params = URL.parse(req.url, true).query;
-
 	var uid = params.uid;
-	var data = TextData[uid] || {
+	var data = UserInfoData[uid] || {
 			avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1495356687735&di=570b69de6951aee4db230ed9ec360d72&imgtype=0&src=http%3A%2F%2Fimg1.3lian.com%2F2015%2Fgif%2Fw%2F63%2F68.jpg",
 			nickName: "嗨",
 			signature: "这个人太懒，什么都没有留下"
@@ -17,6 +17,42 @@ router.get('/getUserInfo', function (req, res, next) {
 	var response = {error_code: 0, data: data};
 	res.send(JSON.stringify(response));
 
+});
+
+router.post('/updateUserInfo', function (req, res) {
+	//没有验证token
+	var response = {error_code: 0};
+	var params = req.body;
+	if (!params || !params.uid) {
+		response.error_code = 1;
+		response.msg = "input error";
+		res.send(JSON.stringify(response));
+		return;
+	}
+
+	var uid = params.uid;
+	if (!UserInfoData[uid]) {
+		response.error_code = 1;
+		response.msg = "user info is not exist";
+		res.send(JSON.stringify(response));
+		return;
+	}
+
+	var dataInfo = UserInfoData[uid];
+	var data = {};
+	dataInfo = Object.assign({}, dataInfo, params);
+	UserInfoData[uid] = dataInfo;
+
+	fs.writeFile('./ApiServer/routes/data/UserInfoData.json', JSON.stringify(UserInfoData), function (err) {
+		if (err) {
+			response.error_code = 2;
+			response.msg = "db error";
+		} else {
+			response.error_code = 0;
+			response.msg = "success";
+		}
+		res.send(JSON.stringify(response));
+	});
 });
 
 module.exports = router;
